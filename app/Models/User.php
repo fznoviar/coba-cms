c14ae2c52a9d84de5ddc7836271cb57d44e91bdf
fznoviar <fznoviar@gmail.com>
2016-11-24 17:21:30 +0700

    Move user model

---
+++ app/Models/User.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Auth\Authenticatable;
+use Illuminate\Auth\Passwords\CanResetPassword;
+use Illuminate\Contracts\Auth\Access\Authorizable as AuthorizableContract;
+use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
+use Illuminate\Contracts\Auth\CanResetPassword as CanResetPasswordContract;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\Access\Authorizable;
+
+class User extends Model implements AuthenticatableContract, AuthorizableContract, CanResetPasswordContract
+{
+    use Authenticatable;
+    use Authorizable;
+    use CanResetPassword;
+
+    /**
+     * The database table used by the model.
+     *
+     * @var string
+     */
+    protected $table = 'users';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'first_name',
+        'last_name',
+        'email',
+        'password',
+        'is_superuser',
+        'is_active',
+        'last_login',
+        'date_joined'
+    ];
+
+    /**
+     * The attributes excluded from the model's JSON form.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password',
+        'remember_token'
+    ];
+
+    protected $dates = [
+        'last_login',
+        'date_joined'
+    ];
+}
