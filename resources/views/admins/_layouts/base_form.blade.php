97c8e9cc29fb5f2777165258a980fa13133e1a8b
fznoviar <fznoviar@gmail.com>
2016-11-24 18:10:20 +0700

    Refactor base form view cms

--- resources/views/admins/_layouts/base_form.blade.php
+++ resources/views/admins/_layouts/base_form.blade.php
@@ -13,36 +13,7 @@
                 <div class="panel-body">
                     <div class="row">
                         <div class="col-lg-12">
-                            <form role="form">
-                                {!! Form::cmsText('nama', 'Nama', null, ['info' => 'Contoh text info tambahan']) !!}
-                                {!! Form::cmsPassword('password', 'Password') !!}
-
-                                {!! Form::cmsNumber('number', 'Number') !!}
-
-                                {!! Form::cmsTextarea('description', 'Description') !!}
-
-                                {!! Form::cmsWysiwyg('content', 'Content') !!}
-
-                                {!! Form::cmsStatic('Static Control', 'email@example.com') !!}
-
-                                {!! Form::cmsSelect('select', 'Select With Placeholder', ['Select 1', 'Select 2'], 'Select option') !!}
-
-                                {!! Form::cmsMultiSelect('multiselect[]', 'Multiselect', [ 1=>'Select 1', 2=>'Select 2', 'Group' => [3=>'Select 3', 4=>'Select 4']])!!}
-
-                                {!! Form::cmsRadio('radio', 'Radio Buttons', [11 => 'Select 1', 12 => 'Select 2']) !!}
-
-                                {!! Form::cmsCheckboxes('checkbox', 'Checkboxes', [11 => 'Select 1', 12 => 'Select 2']) !!}
-
-                                {!! Form::cmsFileBrowser('image', 'Image') !!}
-
-                                <div class="action-form">
-                                    <div class="pull-right">
-                                        {!! Form::cmsSubmit() !!}
-                                        {!! Form::cmsPreview() !!}
-                                        {!! Form::cmsReset() !!}
-                                    </div>
-                                </div>
-                            </form>
+                            @yield('form-fields')
                         </div>
                         <!-- /.col-lg-6 (nested) -->
                     </div>
--- resources/views/admins/_templates/form.blade.php
+++ resources/views/admins/_templates/form.blade.php
@@ -10,3 +10,37 @@
 @section('page-sub-title')
     {{ $flg ? 'Edit' : 'New' }} {{ $page_name }}
 @endsection
+
+@section('form-fields')
+    <form role="form">
+        {!! Form::cmsText('nama', 'Nama', null, ['info' => 'Contoh text info tambahan']) !!}
+        
+        {!! Form::cmsPassword('password', 'Password') !!}
+
+        {!! Form::cmsNumber('number', 'Number') !!}
+
+        {!! Form::cmsTextarea('description', 'Description') !!}
+
+        {!! Form::cmsWysiwyg('content', 'Content') !!}
+
+        {!! Form::cmsStatic('Static Control', 'email@example.com') !!}
+
+        {!! Form::cmsSelect('select', 'Select With Placeholder', ['Select 1', 'Select 2'], 'Select option') !!}
+
+        {!! Form::cmsMultiSelect('multiselect[]', 'Multiselect', [ 1=>'Select 1', 2=>'Select 2', 'Group' => [3=>'Select 3', 4=>'Select 4']])!!}
+
+        {!! Form::cmsRadio('radio', 'Radio Buttons', [11 => 'Select 1', 12 => 'Select 2']) !!}
+
+        {!! Form::cmsCheckboxes('checkbox', 'Checkboxes', [11 => 'Select 1', 12 => 'Select 2']) !!}
+
+        {!! Form::cmsFileBrowser('image', 'Image') !!}
+
+        <div class="action-form">
+            <div class="pull-right">
+                {!! Form::cmsSubmit() !!}
+                {!! Form::cmsPreview() !!}
+                {!! Form::cmsReset() !!}
+            </div>
+        </div>
+    </form>
+@endsection
