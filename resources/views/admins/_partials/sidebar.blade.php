19ef1a07a06229c892dddd59c969f40f0c2a23a3
fznoviar <fznoviar@gmail.com>
2016-11-24 11:01:22 +0700

    Add sidebar for cms

---
+++ resources/views/admins/_partials/sidebar.blade.php
@@ -0,0 +1,12 @@
+<div class="navbar-default sidebar" role="navigation">
+    <div class="sidebar-nav navbar-collapse">
+        <ul class="nav" id="side-menu">
+            <li><span class="sidebar-label">Overview</span></li>
+            <li>
+                <a href="{{ route('backend.index') }}"><i class="fa fa-dashboard fa-fw"></i> Dashboard</a>
+            </li>
+        </ul>
+    </div>
+    <!-- /.sidebar-collapse -->
+</div>
+<!-- /.navbar-static-side -->
