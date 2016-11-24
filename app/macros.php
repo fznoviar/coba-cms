462cab9bd56ce6f3c532db8af58653f8358a088f
fznoviar <fznoviar@gmail.com>
2016-11-24 15:08:04 +0700

    Add macro for form

--- app/macros.php
+++ app/macros.php
@@ -1,2 +1,181 @@
 <?php
 
+Form::macro('cmsField', function ($for, $label, $input, $classes = '', array $option = []) {
+    if (isset($option['info'])) {
+        $info = $option['info'];
+    }
+    $infoValue = isset($info) ? '<span class="help-block">' . $info . '</span>' : '';
+
+    $labelId = generateId($for);
+    $labelValue = ($label !== null) ? $label : '';
+
+    $result = "
+        <div class=\"form-group\">
+            <div class=\"row\">
+                <label class=\"col-md-2\" for=\"$labelId\">$labelValue</label>
+                <div class=\"col-md-10 $classes \">
+                  $input
+                  $infoValue
+                </div>
+            </div>
+        </div>";
+
+    if (isset($option['thumbnail'])) {
+        $url = $option['thumbnail'];
+        $result .= "
+            <div class=\"row\">
+                <div class=\"col-md-offset-2 col-xs-6 col-md-3\">
+                    <a href=\"$url\" target=\"_blank\" class=\"thumbnail\">
+                      <img src=\"$url\" alt=\"$url\">
+                    </a>
+                </div>
+            </div>
+        ";
+    }
+    return $result;
+});
+
+Form::macro('cmsText', function ($name, $label, $value = null, $attributes = []) {
+    addStringToArray('id', generateID($name), $attributes, true);
+    addStringToArray('class', 'form-control', $attributes);
+    addStringToArray('placeholder', "Enter $label", $attributes, true);
+
+    return Form::cmsField($name, $label, Form::text($name, $value, $attributes), '', $attributes);
+});
+
+Form::macro('cmsNumber', function ($name, $label, $value = null, $attributes = []) {
+    addStringToArray('id', generateID($name), $attributes, true);
+    addStringToArray('class', 'form-control', $attributes);
+    addStringToArray('placeholder', "Enter $label", $attributes, true);
+
+    return Form::cmsField($name, $label, Form::input('number', $name, $value, $attributes), '', $attributes);
+});
+
+Form::macro('cmsTextarea', function ($name, $label, $value = null, $attributes = []) {
+    addStringToArray('id', generateID($name), $attributes, true);
+    addStringToArray('class', 'form-control', $attributes);
+    addStringToArray('placeholder', "Enter $label", $attributes, true);
+    addStringToArray('rows', 5, $attributes, true);
+
+    return Form::cmsField($name, $label, Form::textarea($name, $value, $attributes), '', $attributes);
+});
+
+Form::macro('cmsPassword', function ($name, $label, $attributes = []) {
+    addStringToArray('id', generateID($name), $attributes, true);
+    addStringToArray('class', 'form-control', $attributes);
+    addStringToArray('placeholder', "Enter $label", $attributes, true);
+
+    return Form::cmsField($name, $label, Form::password($name, $attributes), '', $attributes);
+});
+
+Form::macro('cmsStatic', function ($label, $value = null, $attributes = []) {
+    $name = lcfirst($label);
+
+    addStringToArray('id', generateID($name), $attributes, true);
+    addStringToArray('class', 'form-control', $attributes);
+
+    $input = "<p>$value</p>";
+
+    return Form::cmsField($name, $label, $input, '', $attributes);
+});
+
+Form::macro('cmsSelect', function ($name, $label, $lists = [], $placeholder, $value = null, $attributes = []) {
+    addStringToArray('id', generateId($name), $attributes, true);
+    addStringToArray('class', 'form-control select2me', $attributes);
+    addStringToArray('data-placeholder', $placeholder, $attributes, true);
+
+    if (!empty($attributes['data-placeholder'])) {
+        $lists = ['' => $attributes['data-placeholder']] + $lists;
+    }
+
+    return Form::cmsField($name, $label, Form::select($name, $lists, $value, $attributes), '', $attributes);
+});
+
+Form::macro(
+    'cmsMultiSelect',
+    function ($name, $label, $lists = [], $placeholder = null, $value = null, $attributes = []) {
+        $attributes['multiple'] = true;
+        return \Form::cmsSelect($name, $label, $lists, $placeholder, $value, $attributes);
+    }
+);
+
+Form::macro('cmsRadio', function ($name, $label, $list = [], $attributes = []) {
+    addStringToArray('id', generateID($name), $attributes, true);
+    addStringToArray('class', 'form-control', $attributes);
+
+    if (!empty($list)) {
+        $input = "";
+        $count = 1;
+        foreach ($list as $value => $valueName) {
+            $radio = Form::radio($name, $value, false, ['id' => $name . $count]);
+            $input .= "
+                <div class=\"radio\">
+                    <label>
+                        $radio $valueName
+                    </label>
+                </div>
+            ";
+            $count++;
+        }
+    }
+
+    return Form::cmsField($name, $label, $input, '', $attributes);
+});
+
+Form::macro('cmsCheckboxes', function ($name, $label, $list = [], $attributes = []) {
+    addStringToArray('id', generateID($name), $attributes, true);
+    addStringToArray('class', 'form-control', $attributes);
+
+    if (!empty($list)) {
+        $input = "";
+        $count = 1;
+        foreach ($list as $value => $valueName) {
+            $checkbox = Form::checkbox($name, $value, false, ['id' => $name . $count]);
+            $input .= "
+                <div class=\"checkbox\">
+                    <label>
+                        $checkbox $valueName
+                    </label>
+                </div>
+            ";
+            $count++;
+        }
+    }
+
+    return Form::cmsField($name, $label, $input, '', $attributes);
+});
+
+Form::macro('cmsSubmit', function ($value = null, $name = null, $attributess = []) {
+    addStringToArray('class', 'btn btn-success', $attributes);
+    return Form::input('submit', $name, $value, $attributes);
+});
+
+\Form::macro('cmsReset', function ($value = null, $attributess = []) {
+    addStringToArray('class', 'btn btn-warning', $attributes);
+    return Form::reset($value, $attributes);
+});
+
+Form::macro('cmsPreview', function ($value = null, $name = null, $attributess = []) {
+    $name = $name ? $name : 'Preview';
+    $value = $value ? $value : '#';
+    return "
+        <a href=\"$value\" class=\"btn btn-primary\">$name</a>
+    ";
+});
+
+Form::macro('cmsModel', function ($model, array $attributes = []) {
+    addStringToArray('class', 'form-horizontal', $attributes);
+
+    if (isset($attributes['prefix'])) {
+        $attributes['route'] = $model->exists
+            ? [suitRouteName($attributes['prefix'] . '.update'), $model]
+            : suitRouteName($attributes['prefix'] . '.store');
+        unset($attributes['prefix']);
+    }
+
+    if (!isset($attributes['method'])) {
+        $attributes['method'] = $model->exists ? 'PUT' : 'POST';
+    }
+
+    return Form::model($model, $attributes);
+});
--- resources/views/admins/_layouts/base_form.blade.php
+++ resources/views/admins/_layouts/base_form.blade.php
@@ -14,33 +14,22 @@
                     <div class="row">
                         <div class="col-lg-12">
                             <form role="form">
-                                <div class="form-group">
-                                    <div class="row">
-                                        <label class="col-md-2">Text Input with Placeholder</label>
-                                        <div class="col-md-10">
-                                            <input class="form-control" placeholder="Enter text">
-                                            {{-- <p class="help-block">Example block-level help text here.</p> --}}
-                                        </div>
-                                    </div>
-                                </div>
+                                {!! Form::cmsText('nama', 'Nama', null, ['info' => 'Contoh text info tambahan']) !!}
+                                {!! Form::cmsPassword('password', 'Password') !!}
 
-                                <div class="form-group">
-                                    <div class="row">
-                                        <label class="col-md-2">Text area</label>
-                                        <div class="col-md-10">
-                                            <textarea class="form-control" rows="6"></textarea>
-                                        </div>
-                                    </div>
-                                </div>
+                                {!! Form::cmsNumber('number', 'Number') !!}
 
-                                <div class="form-group">
-                                    <div class="row">
-                                        <label class="col-md-2">Static Control</label>
-                                        <div class="col-md-10">
-                                            <p>email@example.com</p>
-                                        </div>
-                                    </div>
-                                </div>
+                                {!! Form::cmsTextarea('description', 'Description') !!}
+
+                                {!! Form::cmsStatic('Static Control', 'email@example.com') !!}
+
+                                {!! Form::cmsSelect('select', 'Select With Placeholder', ['Select 1', 'Select 2'], 'Select option') !!}
+
+                                {!! Form::cmsMultiSelect('multiselect[]', 'Multiselect', [ 1=>'Select 1', 2=>'Select 2', 'Group' => [3=>'Select 3', 4=>'Select 4']])!!}
+
+                                {!! Form::cmsRadio('radio', 'Radio Buttons', [11 => 'Select 1', 12 => 'Select 2']) !!}
+
+                                {!! Form::cmsCheckboxes('checkbox', 'Checkboxes', [11 => 'Select 1', 12 => 'Select 2']) !!}
 
                                 <div class="form-group">
                                     <div class="row">
@@ -51,84 +40,13 @@
                                     </div>
                                 </div>
 
-                                <div class="form-group">
-                                    <div class="row">
-                                        <label class="col-md-2">Checkboxex</label>
-                                        <div class="col-md-10">
-                                            <div class="checkbox">
-                                                <label>
-                                                    <input type="checkbox" value="">Checkbox 1
-                                                </label>
-                                            </div>
-                                            <div class="checkbox">
-                                                <label>
-                                                    <input type="checkbox" value="">Checkbox 2
-                                                </label>
-                                            </div>
-                                            <div class="checkbox">
-                                                <label>
-                                                    <input type="checkbox" value="">Checkbox 3
-                                                </label>
-                                            </div>
-                                        </div>
+                                <div class="action-form">
+                                    <div class="pull-right">
+                                        {!! Form::cmsSubmit() !!}
+                                        {!! Form::cmsPreview() !!}
+                                        {!! Form::cmsReset() !!}
                                     </div>
                                 </div>
-
-                                <div class="form-group">
-                                    <div class="row">
-                                        <label class="col-md-2">Radio Buttons</label>
-                                        <div class="col-md-10">
-                                            <div class="radio">
-                                                <label>
-                                                    <input type="radio" name="optionsRadios" id="optionsRadios1" value="option1" checked>Radio 1
-                                                </label>
-                                            </div>
-                                            <div class="radio">
-                                                <label>
-                                                    <input type="radio" name="optionsRadios" id="optionsRadios2" value="option2">Radio 2
-                                                </label>
-                                            </div>
-                                            <div class="radio">
-                                                <label>
-                                                    <input type="radio" name="optionsRadios" id="optionsRadios3" value="option3">Radio 3
-                                                </label>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-
-                                <div class="form-group">
-                                    <div class="row">
-                                        <label class="col-md-2">Selects</label>
-                                        <div class="col-md-10">
-                                            <select class="form-control">
-                                                <option>1</option>
-                                                <option>2</option>
-                                                <option>3</option>
-                                                <option>4</option>
-                                                <option>5</option>
-                                            </select>
-                                        </div>
-                                    </div>
-                                </div>
-
-                                <div class="form-group">
-                                    <div class="row">
-                                        <label class="col-md-2">Multiple Selects</label>
-                                        <div class="col-md-10">
-                                            <select multiple class="form-control">
-                                                <option>1</option>
-                                                <option>2</option>
-                                                <option>3</option>
-                                                <option>4</option>
-                                                <option>5</option>
-                                            </select>
-                                        </div>
-                                    </div>
-                                </div>
-
-                                <button type="submit" class="btn btn-primary">Submit Button</button>
-                                <button type="reset" class="btn btn-warning">Reset Button</button>
                             </form>
                         </div>
                         <!-- /.col-lg-6 (nested) -->
